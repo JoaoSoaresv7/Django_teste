ecf326744a380afe734bc976a3131ebe306c396c
Joao Vitor <joaovitor@Gabriels-MacBook-Air.local>
2025-04-25 18:04:36 -0300

    correção

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -10,10 +10,10 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
+
     @admin.display(
         boolean=True,
         ordering="pub_date",
@@ -23,6 +23,7 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -30,9 +31,10 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-    
+
+
 def was_published_recently(self):
     now = timezone.now()
     return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-    
+# Create your models here.
